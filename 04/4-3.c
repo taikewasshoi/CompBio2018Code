31d10a3038d8ec8dce11ae4cce923494f4233415
taikewasshoi <38200812+taikewasshoi@users.noreply.github.com>
2018-05-21 11:03:54 +0900

    Add files via upload

---
+++ 04/4-3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(void){
+  int i,j;
+  for(i=0;i<10;i++){
+    j=0;
+    while(j<i){
+      printf("True");
+      j++;
+    }
+    printf("\n");
+  }
+    return 0;
+  }
---
+++ 04/4-4.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void){
+  int i;
+  double d;
+  i=5;
+  d=i;
+  printf("%d,%f\n",i,d);
+  
+  d=3.6;
+  i=d;
+  printf("%d,%f\n",i,d);
+  
+  return 0;
+}
